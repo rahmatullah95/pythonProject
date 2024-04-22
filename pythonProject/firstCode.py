23873d24c331c2d800c938ac8dcd280bfa2dbda0
Rahmat Ullah <rahmatbd1533@gmail.com>
2024-04-22 11:49:14 +0600

    Python

---
+++ pythonProject/firstCode.py
@@ -0,0 +1,13 @@
+print(23)
+print('Rahmat Ullah')
+
+a = 2
+b = 3
+c = a + b
+z = a - b
+e = a * b
+d = a / b
+print(c)
+print(z)
+print(e)
+print(d)
---
+++ pythonProject/variables.py
@@ -0,0 +1,4 @@
+name = 'Rahmat Ullah'
+
+print('My name is', name)
+print("Me", name, "and Robin were going to school together")
