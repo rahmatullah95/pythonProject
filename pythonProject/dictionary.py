8bac732b691d3bcbfccb420041fe5f3af0f2dd84
Rahmat Ullah <rahmatbd1533@gmail.com>
2024-04-25 14:32:49 +0600

    Merge branch 'files'
    # Conflicts:
    #	pythonProject/stringFormatting.py

---
+++ pythonProject/dictionary.py
@@ -0,0 +1,11 @@
+studenId = {
+    69 : 'Shadman Ahmed',
+    74 : 'Rahmat Ullah',
+    78 : 'Shanto',
+    91 : 'Helal',
+    106 : 'Ratul',
+    130 : 'Shontosh'
+}
+
+print(studenId[74])
+print(studenId.get(129), "\nNot a veliid key")
